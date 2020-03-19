fd3deca936d6923412ea092461ef1fc57e941af3
Darren Hoyland <darren@hoyland.me>
2020-03-19 21:11:49 +0000

    Gunicorn and Dockerfile

--- api/api/App.py
+++ api/api/App.py
@@ -2,13 +2,16 @@
 # encoding: UTF-8
 
 import json
+from pathlib import Path
 from flask import Flask, jsonify
 
 
 app = Flask(__name__)
 
+# breakpoint()
+
 def getJSON(filename):
-    with open(f"{filename}.json", 'r') as f:
+    with open(f"{Path(__file__).parent}/{filename}.json", 'r') as f:
         return json.loads(f.read())
 
 @app.route('/resume')
@@ -17,3 +20,4 @@ def resume():
     response = jsonify(return_value)
     response.status_code = 200
     return response
+
--- api/run_dev.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "*** Running app via gunicorn ***"
-
-gunicorn3 App:app
-
-echo "------------------------------------------------------------------------"
